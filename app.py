f397e485c2dabbeac7f63bb0670c3adda77c5cd9
Xaponquel <130245408+xaponquel@users.noreply.github.com>
2023-06-13 19:35:50 +0700

    Update app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+def main():
+    st.title("Selamat Datang")
+    st.subheader("Halo, saya Melynda Isaura NRP 2043221015")
+    
+    st.subheader("Daftar")
+    st.write("Pilihan :")
+    st.write("- Unifrom Kontinu")
+    st.write("- Z-test")
+    st.write("- Exponensial")
+    st.write("- Matrix")
+    st.write("- Regresi Linier")
+    
+
+if __name__ == "__main__":
+    main()
